4f1aaca95c6f2cbff4484efd6b674582c5c111c6
HaaaamBurger <gemasclashes@gmail.com>
2023-11-07 23:08:16 +0200

    #

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -4,4 +4,9 @@ config();
 
 export const configs = {
     API_PORT: process.env.API_PORT,
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_S3_REGION: process.env.AWS_S3_REGION,
+    AWS_S3_BUCKET: process.env.AWS_S3_BUCKET,
+    API_DATABASE_URI: process.env.API_DATABASE_URI
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import express from "express";
-import { configs } from "./configs/config";
+import * as mongoose from "mongoose";
+import { configs } from "./configs";
+import { userRouter } from "./routers";
+
+
+
 
 const main = express();
 
 main.use(express.json());
 main.use(express.urlencoded({extended: true}));
 
+main.use("users", userRouter)
+
 main.listen(configs.API_PORT, async() => {
+    await mongoose.connect(configs.API_DATABASE_URI);
     console.log(`Server has started on ${configs.API_PORT} port!`);
 })
