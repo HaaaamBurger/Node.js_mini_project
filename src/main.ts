c202ebb10cc9186f2209c2f956a3fe7a7d1e8b7b
HaaaamBurger <gemasclashes@gmail.com>
2023-11-07 19:11:56 +0200

    #

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ const main = express();
 main.use(express.json());
 main.use(express.urlencoded({extended: true}));
 
-
 main.listen(configs.API_PORT, async() => {
     console.log(`Server has started on ${configs.API_PORT} port!`);
 })
