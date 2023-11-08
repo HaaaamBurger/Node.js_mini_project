a4a2dfb89e016fc40e7d3118838a96d31afb30bb
HaaaamBurger <gemasclashes@gmail.com>
2023-11-08 12:36:47 +0200

    #

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import express from "express";
 import * as mongoose from "mongoose";
+
+import { Request, Response, NextFunction } from "express";
 import { configs } from "./configs";
-import { userRouter } from "./routers";
-
-
-
+import { ApiError } from "./errors";
+import { authRouter } from "./routers";
 
 const main = express();
 
 main.use(express.json());
 main.use(express.urlencoded({extended: true}));
 
-main.use("users", userRouter)
+main.use("/auth", authRouter)
+
+main.use((error: ApiError, req: Request, res: Response, next: NextFunction) => {
+    const status = error.status || 500;
+
+    res.status(status).json({
+        message: error.message,
+        status: error.status
+    })
+})
 
 main.listen(configs.API_PORT, async() => {
     await mongoose.connect(configs.API_DATABASE_URI);
