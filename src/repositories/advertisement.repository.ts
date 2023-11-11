958afdbc4076e0ed096022079e86b11ca214d03f
HaaaamBurger <gemasclashes@gmail.com>
2023-11-11 14:05:34 +0200

    #

---
+++ src/repositories/advertisement.repository.ts
@@ -0,0 +1,38 @@
+import { ApiError } from "../errors";
+import { IExchnage } from "../interfaces";
+import { Currency } from "../models";
+
+
+class AdvertisementRepository {
+   public async convertCurrency(currency: IExchnage) {
+       try {
+           const currencies = await Currency.findOne();
+
+           console.log(currency.price,currencies.usd_ccy);
+           switch (currency.base_ccy) {
+               case "UAH":
+                   return {
+                       UAH: currency.price,
+                       USD: Number((Number(currency.price) / Number(currencies.usd_ccy)).toFixed(5)),
+                       EUR: Number((Number(currency.price) / Number(currencies.eur_ccy)).toFixed(5)),
+                   }
+               case "USD":
+                   return {
+                       USD: currency.price,
+                       UAH: Number((Number(currency.price) * Number(currencies.usd_ccy)).toFixed(5)),
+                       EUR: Number((Number(currency.price) * Number(currencies.eur_ccy) / Number(currencies.usd_ccy)).toFixed(5)),
+                   }
+               case "EUR":
+                   return {
+                       EUR: currency.price,
+                       USD: Number((Number(currency.price) * Number(currencies.usd_ccy) / Number(currencies.eur_ccy)).toFixed(5)),
+                       UAH: Number((Number(currency.price) * Number(currencies.usd_ccy)).toFixed(5))
+                   }
+           }
+       } catch (e) {
+           throw new ApiError(e.message, e.status);
+       }
+   } 
+}
+
+export const advertisementRepository = new AdvertisementRepository();
